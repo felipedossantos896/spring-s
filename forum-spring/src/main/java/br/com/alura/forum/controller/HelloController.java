ebada465f8c45883a313ac62064aec1f019dcd17
Felipe Carneiro <felipedosssantos896@gmail.com>
2022-06-09 12:43:39 -0300

    adicionando o projeto spring-forum

---
+++ forum-spring/src/main/java/br/com/alura/forum/controller/HelloController.java
@@ -0,0 +1,15 @@
+package br.com.alura.forum.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String hello() {
+        return "Hello World";
+    }
+}

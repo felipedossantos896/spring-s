011187dbc713f1b5d740b41da613bbf030895da7
Felipe Carneiro <felipedosssantos896@gmail.com>
2022-07-05 19:39:45 -0300

    [feat]: add caching

--- forum-spring/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum-spring/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -7,8 +7,8 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
@@ -43,6 +43,7 @@ public class TopicosController {
     private CursoRepository cursoRepository;
 
     @GetMapping
+    @Cacheable(value = "listaDeTopicos")
     public Page<TopicoDto> lista(
         @RequestParam(required = false) String nomeCurso, 
         @PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 10)

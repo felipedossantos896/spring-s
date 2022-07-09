496a75294359563b320b8a4e7f7ccc87027a5425
Felipe Carneiro <felipedosssantos896@gmail.com>
2022-07-08 21:15:43 -0300

    feat: implementation authentication and authorization

--- forum-spring/src/main/resources/data.sql
+++ forum-spring/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$qeDkkeShqUo9gd0gdIaa4.6sxDU52zeZBV8JeZB5O90qftCQqVtE6');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
